82c12fb8280ca8b34a3bc9d0863521c778231033
daria-levit <55983425+daria-levit@users.noreply.github.com>
2020-01-05 18:15:06 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app
+          id="app"
+          class="mx-0"
+  >
+      <v-content class="pt-5">
+          <MainComponent/>
+      </v-content>
+    </v-app>
+</template>
+
+<script>
+import MainComponent from "./components/MainComponent";
+
+export default {
+  name: 'App',
+  components: {
+    MainComponent
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+
+html {
+height : 100%;
+}
+#app {
+background        : linear-gradient(45deg, #1d1838, #d0b5e8, #85ccec, #02335d);
+background-repeat : no-repeat;
+font-family       : Arial, sans-serif;
+}
+
+</style>
---
+++ InputComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="input d-flex justify-center my-7 mx-1">
+            <v-text-field
+                    class="pa-0"
+                    v-model="someTask"
+                    @keypress.enter.prevent="addTask"
+                    placeholder="add to the list"
+                    type="text"
+                    name="textview"
+                    solo
+                    dense
+            ></v-text-field>
+
+            <v-btn
+                    class="ml-4"
+                    normal
+                    @click="addTask"
+            >
+                OK
+            </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'InputComponent',
+        data: () => ({
+            someTask: '',
+        }),
+        methods: {
+            addTask() {
+                this.$emit('addTask', this.someTask);
+                this.someTask = '';
+            },
+        },
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input{
+    max-height: 36px;
+}
+</style>
